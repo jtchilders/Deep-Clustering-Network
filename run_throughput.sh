6027e9b21e3ebba8d7398a6fe33a63495826b8e8
Evangelos Kourlitis <evangelos.kourlitis@cern.ch>
2021-10-18 04:52:41 -0500

    minimal eddits

--- run_throughput.sh
+++ run_throughput.sh
@@ -2,7 +2,7 @@
 
 for BATCH in 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
 do
-   echo "Testing batch-size: $BATCH"
+   echo "Measuring with batch-size: $BATCH"
    python train_simple_ae.py --data ~whopkins/sigclustering/sigclustering/ccMET_noBackground.h5 --time-throughput --batch-size $BATCH > log_$BATCH.txt
    echo "Done!"
 done
